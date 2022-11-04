bb533f7bc921a82dc4e83cb652fd5d58d83e067a
colbychung01 <colbychung01@gmail.com>
2022-11-04 01:39:45 -0400

    Update script.js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -112,7 +112,7 @@ async function mainEvent() {
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (!arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
     submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     loadAnimation.classList.remove('lds-ellipsis');

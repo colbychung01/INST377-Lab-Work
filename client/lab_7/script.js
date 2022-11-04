0c92804c1d3eee944b9ca59dcd1382cd8785c8f4
colbychung01 <colbychung01@gmail.com>
2022-11-04 17:07:07 -0400

    yes

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -74,6 +74,15 @@ function processRestaurants(list) {
   */
 }
 
+function filterList(array, filterInputValue) {
+  return list.filter((item) => {
+    if (item.name) { return; }
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = filterInputValue.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+}
+
 async function mainEvent() {
   /*
     ## Main Event
@@ -112,50 +121,36 @@ async function mainEvent() {
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+  if (!arrayFromJson.data?.length > 0) { return; } // the question mark in this means "if this is set at all"  
+  let currentList = [];
+  submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
-    loadAnimation.classList.remove('lds-ellipsis');
-    loadAnimation.classList.add('lds-ellipsis_hidden');
+  loadAnimation.classList.remove('lds-ellipsis');
+  loadAnimation.classList.add('lds-ellipsis_hidden');
 
-    let currentList = [];
+  form.addEventListener('input', (event) => {
+    console.log('input', event.target.value);
+    const filteredList = filterList(currentList, event.target.value);
+    injectHTML(filteredList);
+  });
+  // And here's an eventListener! It's listening for a "submit" button specifically being clicked
+  // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
+  form.addEventListener('submit', (submitEvent) => {
+    // This is needed to stop our page from changing to a new URL even though it heard a GET request
+    submitEvent.preventDefault();
 
-    form.addEventListener('input', (event) => {
-      console.log(event.target.value);
-      const filteredList = filterList(currentList, event.target.value);
-      injectHTML(filteredList);
-    });
+    // This constant will have the value of your 15-restaurant collection when it processes
+    currentList = processRestaurants(arrayFromJson.data);
+    console.log(currentList);
+    // And this function call will perform the "side effect" of injecting the HTML list for you
+    injectHTML(currentList);
 
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
-      submitEvent.preventDefault();
-
-      // This constant will have the value of your 15-restaurant collection when it processes
-      currentList = processRestaurants(arrayFromJson.data);
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(currentList);
-
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
-    });
-  }
-}
-
-function processRestaurants(list) {
-
-}
-
-function filterList(array, filterInputValue) {
-    return array.filter((item) => {
-        if (!item.name) { return; }
-        const lowerCaseName = item.name.toLowerCase();
-        const lowerCaseQuery = item.name.toLowerCase();
-        return lowerCaseName.includes(lowerCaseQuery);
+    // By separating the functions, we open the possibility of regenerating the list
+    // without having to retrieve fresh data every time
+    // We also have access to some form values, so we could filter the list based on name
   });
 }
+
 /*
   This last line actually runs first!
   It's calling the 'mainEvent' function at line 57
